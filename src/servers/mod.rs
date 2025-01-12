1b7a813fa808f430aaa6fe10cac42cee12c20fb1
tawansongsang <tawansongsang.k@gmail.com>
2025-01-12 10:57:39 +0700

    add docs

--- src/servers/mod.rs
+++ src/servers/mod.rs
@@ -1,7 +1,1 @@
-use dioxus::prelude::*;
 
-/// Echo the user input on the server.
-#[server(EchoServer)]
-pub async fn echo_server(input: String) -> Result<String, ServerFnError> {
-    Ok(input)
-}
